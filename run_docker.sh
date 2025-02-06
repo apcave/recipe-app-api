0afbe08db50811d5314ebc6bb27679fdd9c1a769
Alex Cave <alex.cave@outlook.com.au>
2025-02-06 14:35:26 +1100

    Fixed Error in Actions

--- run_docker.sh
+++ run_docker.sh
@@ -2,24 +2,24 @@
 
 
 sudo docker build .
-sudo docker-compose build
+sudo docker compose build
 
 # Linting with flake8
-sudo docker-compose run --rm app sh -c "flake8 ."
+sudo docker compose run --rm app sh -c "flake8 ."
 
 # Run tests
-sudo docker-compose run --rm app sh -c "python manage.py test"
+sudo docker compose run --rm app sh -c "python manage.py test"
 
 
-sudo docker-compose run --rm app sh -c "python manage.py collectstatic"
+sudo docker compose run --rm app sh -c "python manage.py collectstatic"
 
 
 # Get CLI
-sudo docker-compose run --rm app sh
+sudo docker compose run --rm app sh
 
 # Create a new Django project.
 # sudo docker-compose run --rm app sh -c "django-admin startproject app ."
 
 
 # Starts the web service
-sudo docker-compose up
+sudo docker compose up
