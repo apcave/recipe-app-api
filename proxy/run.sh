9dc9cdc7e477f78738f9072ba9c7d15b97ec5a22
Alex Cave <alex.cave@outlook.com.au>
2025-02-09 00:57:55 +1100

    bug 3

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,5 +3,5 @@
 set -e
 
 # Substitute environment variables in the .conf! Very handy...
-envsubst < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
