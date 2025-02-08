b8072b739550a87df95d434767c1521ffcf073e0
Alex Cave <alex.cave@outlook.com.au>
2025-02-08 19:42:08 +1100

    Ready to configure AWS. :)

---
+++ proxy/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+# Substitute environment variables in the .conf! Very handy...
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
