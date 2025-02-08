896cedf481696f4a7b5de6562e9dea329645b609
Alex Cave <alex.cave@outlook.com.au>
2025-02-09 01:40:35 +1100

    bug 5

--- app/app/urls.py
+++ app/app/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
     ),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-    path('', views.home, name='home'),
 ]
 
 if settings.DEBUG:
